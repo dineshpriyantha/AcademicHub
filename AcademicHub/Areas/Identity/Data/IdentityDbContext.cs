6287bd482de4ef1ebc8663754173a90a2ee29743
dineshpriyantha <dinesh.priyantha.uom@gmail.com>
2023-06-22 01:13:21 +0900

    Add IdentityUser

---
+++ AcademicHub/Areas/Identity/Data/IdentityDbContext.cs
@@ -0,0 +1,34 @@
+﻿using AcademicHub.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AcademicHub.Data;
+
+public class IdentityDbContext : IdentityDbContext<AcademicHubUser>
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<AcademicHubUser>
+{
+    public void Configure(EntityTypeBuilder<AcademicHubUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+    }
+}
--- AcademicHub/Program.cs
+++ AcademicHub/Program.cs
@@ -3,12 +3,19 @@ using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using AcademicHub.Data;
+using AcademicHub.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSqlServer<AcademicHubDbContext>(builder.Configuration.GetConnectionString("connectionstr"));
+
+builder.Services.AddSqlServer<IdentityDbContext>(builder.Configuration.GetConnectionString("connectionstr"));
+
+builder.Services.AddDefaultIdentity<AcademicHubUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<IdentityDbContext>();
 
 var app = builder.Build();
 
@@ -31,4 +38,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
